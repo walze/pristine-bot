6ecc380065eb6877a714cbb0ec397573eff182e5
Walze <catufuzgu@gmail.com>
2018-05-02 21:01:46 -0300

    added image command

--- src/commands/declarations.ts
+++ src/commands/declarations.ts
@@ -27,13 +27,18 @@ const say = new Command('say', (msg, params) => {
   msg.channel.send(`${params.getAt(0).tag} ${params.text}`)
 })
 
-const thonk = new Command('thonk', msg => {
+const thonkwot = new Command('thonkwot', msg => {
   msg.delete()
   msg.channel.send('', { file: 'https://cdn.discordapp.com/emojis/409528321685061634.png' })
 })
 
-const help = new Command('help', msg => msg.channel.send(commands.list()))
+const image = new Command('image', (msg, params) => {
+  msg.delete()
+  msg.channel.send('', { file: params.text })
+})
 
+const help = new Command('help', msg => msg.channel.send(commands.list()))
+//https://boards.fscchan.nl/images/22169.jpg
 const audits = new Command('audits', auditsAction)
 const tl = new Command('tl', translatorAction)
 const urban = new Command('urban', urbanAction)
@@ -42,10 +47,11 @@ const declarations: Command[] = [
   debug,
   audits,
   tl,
-  thonk,
+  thonkwot,
   say,
   urban,
   help,
+  image
 ]
 
 export default declarations
