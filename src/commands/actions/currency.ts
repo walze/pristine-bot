8bec17ab0c2b31f5886a773908bd7481d3667f28
Walze <catufuzgu@gmail.com>
2018-05-04 16:53:03 -0300

    error handling and added codes

--- src/commands/actions/currency.ts
+++ src/commands/actions/currency.ts
@@ -9,25 +9,33 @@ export interface currencyParams {
 }
 
 export const currencyAction: DiscordAction<currencyParams> = (msg, params) => {
-  params.from = params.from.toUpperCase()
-  params.to = params.to.toUpperCase()
+  if (params.text.indexOf('codes') > -1) {
+    msg.channel.send('https://www.xe.com/iso4217.php')
+    return null
+  }
 
-  const fromTo = `${params.from}_${params.to}`
+  if (Boolean(params.from) && Boolean(params.to)) {
+    params.from = params.from.toUpperCase()
+    params.to = params.to.toUpperCase()
 
-  Axios.get(`https://free.currencyconverterapi.com/api/v5/convert?q=${fromTo}&compact=y`)
-    .then(res => {
-      const val = res.data[fromTo].val
-      const multiplier = Number(params.text)
-      if (Boolean(multiplier))
-        msg.channel.send(`${params.text} ${params.from} = ${val * multiplier} ${params.to}`)
-      else
-        msg.channel.send(`1 ${params.from} = ${val} ${params.to}`)
-    })
-    .catch((err: AxiosError) => {
-      log(err.response, err.message)
+    const fromTo = `${params.from}_${params.to}`
 
-      msg.channel.send(`Not Found
+
+    Axios.get(`https://free.currencyconverterapi.com/api/v5/convert?q=${fromTo}&compact=y`)
+      .then(res => {
+        const val = res.data[fromTo].val
+        const multiplier = Number(params.text)
+        if (Boolean(multiplier))
+          msg.channel.send(`${params.text} ${params.from} = ${val * multiplier} ${params.to}`)
+        else
+          msg.channel.send(`1 ${params.from} = ${val} ${params.to}`)
+      })
+      .catch((err: AxiosError) => {
+        log(err.response, err.message)
+
+        msg.channel.send(`Not Found
 ${err.message}`)
-    })
-}
+      })
+  } else msg.channel.send('Wrong syntax. s-curr from-\"CURRENCY_CODE\" to-\"CURRENCY_CODE\" \"VALUE OR EMPTY\"')
 
+}
