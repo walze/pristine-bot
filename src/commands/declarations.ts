afc7cffe751e6839778779ebacde9e6cd794b54c
Yuri Renê Ferreira Rosino <yrosino@brtelecom.com.br>
2018-05-07 14:39:26 -0300

    added type to declarations

--- src/commands/declarations.ts
+++ src/commands/declarations.ts
@@ -12,19 +12,20 @@ import SmallActions from './smallActions'
 // Eg. s-debug event-MEMBER_ADD_BAN amount-5 @wiva#9996
 // params.argument will equals to value
 
-const Declarations = [
-  new Command('debug', SmallActions.debug),
-  new Command('say', SmallActions.say),
-  new Command('thonkwot', SmallActions.thonkwot),
-  new Command('image', SmallActions.image),
-  new Command('help', msg => msg.channel.send(Commands.list())),
-  new Command('audits', auditsAction),
-  new Command('tl', translatorAction),
-  new Command('urban', urbanAction),
-  new Command('define', defineAction),
-  new Command('google', googleAction),
-  new Command('mafs', SmallActions.mafs),
-  new Command('curr', currencyAction)
-]
+const Declarations: Command[] =
+  [
+    new Command('debug', SmallActions.debug),
+    new Command('say', SmallActions.say),
+    new Command('thonkwot', SmallActions.thonkwot),
+    new Command('image', SmallActions.image),
+    new Command('help', msg => msg.channel.send(Commands.list())),
+    new Command('audits', auditsAction),
+    new Command('tl', translatorAction),
+    new Command('urban', urbanAction),
+    new Command('define', defineAction),
+    new Command('google', googleAction),
+    new Command('mafs', SmallActions.mafs),
+    new Command('curr', currencyAction)
+  ]
 
 export default Declarations
