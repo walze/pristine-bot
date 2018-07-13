8dcb5276c502739d19cf8df082b73847624e18b6
Yuri Renê Ferreira Rosino <yrosino@brtelecom.com.br>
2018-07-13 14:10:26 -0300

    fixed barrel wrong path

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Client } from 'discord.js'
 import log from './bot/helpers/logger'
 import Request from './bot/classes/Request'
 import Commands from './bot/classes/Commands'
-import './commands/barrel'
+import './bot/commands/barrel'
 
 const client = new Client()
 
