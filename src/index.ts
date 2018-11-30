2c4805697bc9069d00c2f27bf4648561996b16b3
Walze <catufuzgu@gmail.com>
2018-11-30 17:00:24 -0200

    removed performance test

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
-import { Message } from 'discord.js';
+import { Message } from 'discord.js'
 // tslint:disable-next-line:no-var-requires
-require('source-map-support').install();
+require('source-map-support').install()
 process.setMaxListeners(0)
 
 import CommandRequest from './bot/classes/CommandRequest'
 import WordsMod from './database/classes/WordsMod'
 import client from './setup'
-import Performances from './bot/classes/Performances'
 
 import ReplyError from './bot/helpers/ReplyError'
 import Commands from './bot/classes/Commands'
 import { MessageAverage } from './database/classes/MessageAverage'
 
-const runTasks = async (msg: Message) => {
+const runTasks = (msg: Message) => {
 
   // returns if msg is from bot
-  if (msg.author.id === msg.client.user.id) return 0
-
-  // starts performance test
-  Performances.start('request')
+  if (msg.author.id === msg.client.user.id) return
 
   // creates new request
   const request = new CommandRequest(msg)
@@ -26,41 +22,19 @@ const runTasks = async (msg: Message) => {
   // Handles Request Errors
   try {
 
-    let t1 = 0
-    let t2 = 0
-    let t3 = 0
-
-    // if there is a command
     if (request.command) {
-      console.log('\n')
-
-      // ends request p-test
-      t1 = Performances.end('request')
-
-      Performances.start('command')
-
-      // Executes command
-      await Commands.execute(request)
-
-      // ends command p-test after run
-      t2 = Performances.end('command')
+      Commands.execute(request)
     }
-
-    Performances.start('wordsmod')
 
     // creates wordsmod and emits it
     const mod = new WordsMod(request)
     mod.emit()
+
     // tslint:disable-next-line:no-unused-expression
     new MessageAverage(msg)
 
-    if (request.command)
-      t3 = Performances.end('wordsmod')
-
-    return [t1, t2, t3].reduce((prev, curr) => prev + curr)
   } catch (err) {
     ReplyError(request, err)
-    return 0
   }
 }
 
@@ -68,20 +42,10 @@ const onMessage = async (msg: Message) => {
   // Handles Internal Errors
   try {
 
-    // starts performance test for everything
-    Performances.start('all');
+    runTasks(msg)
 
-    const times = await runTasks(msg);
-
-    if (!times || times < 1)
-      return;
-
-    // ends p-test of all but gets time only
-    const t3 = Performances.end('all', false);
-
-    console.log(`|| everything ran in ${times + t3} ms \n`);
   } catch (err) {
-    ReplyError(msg, err);
+    ReplyError(msg, err)
   }
 }
 
