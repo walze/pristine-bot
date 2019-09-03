4504e2feb3a9d542793c8e2d247500449958d689
Wallace Fares <wallace.fares@webedia-group.com>
2019-09-03 15:57:27 -0300

    idk

--- src/bot/parse.ts
+++ src/bot/parse.ts
@@ -1,6 +1,7 @@
 import { changeCommand, ICommand, getCommand } from './command';
 import { Map } from 'immutable';
 import { Actions } from './actions/helpers/enum';
+import matchAll from 'string.prototype.matchall'
 
 export interface IArgument {
   match?: RegExpMatchArray,
@@ -8,44 +9,9 @@ export interface IArgument {
   value?: string,
 }
 
-const filterArgs = (
-  acc: IArgument[],
-  string: string,
-  index: number,
-  array: string[],
-) => {
-  const arg = string.match(/--(\w+)/)
-
-  if (arg) {
-    const [, key] = arg
-    const value = array[index + 1]
-
-    return [
-      ...acc,
-      { match: arg, key, value },
-    ]
-  }
-
-  const previous = acc[index - 1]
-
-  if ((previous && previous.match) || arg) {
-    delete acc[index - 1].match
-    return [...acc]
-  }
-
-  return [
-    ...acc,
-    { key: 'message', value: string },
-  ]
-}
-
-const filterMessage = (args: IArgument[]) => args
-  .filter(arg => arg.key === 'message')
-  .map(arg => arg.value)
-  .join(' ')
+const flagRegex = /--(\w+)[=\s]?(\w+)?/g
 
 const filterFlags = (args: IArgument[]) => args
-  .filter(arg => arg.key !== 'message')
   .reduce(
     (acc, arg) => {
       if (!arg.key || !arg.value) return acc;
@@ -67,11 +33,11 @@ export const parseCommand = (command: ICommand) => {
 
   if (pristine.toLowerCase().trim() !== 'pristine,') return command;
 
-  const args = split.reduce(filterArgs, [] as IArgument[])
-  const userMessage = filterMessage(args)
-  const flags = Map(filterFlags(args))
+  const joined = split.join(' ')
 
-  // console.log(args)
+  const args = [...matchAll(joined, flagRegex)].map(([, key, value]) => ({ key, value }))
+  const userMessage = joined.replace(flagRegex, '')
+  const flags = Map(filterFlags(args))
 
   return changeCommand(
     command,
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,8 @@ declare module 'better-log';
 declare module 'jsdom';
 declare module "*.json"
 declare module "*.txt"
+
+
+declare module "string.prototype.matchall" {
+  export default function matchAll(string: string, regex: RegExp): RegExpMatchArray[]
+}
