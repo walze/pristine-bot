0c4ff14876d82459226bb1ac4e96d0039c1eefd8
wallace fares <wallacefares@gmail.com>
2022-11-02 07:42:41 -0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,50 @@
-import { config } from "dot-env";
-import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
-import assert from "assert";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-config();
-const { TOKEN, CLIENT_ID } = process.env;
-assert(TOKEN, "TOKEN is not defined");
-assert(CLIENT_ID, "CLIENT_ID is not defined");
+import {Client, GatewayIntentBits, REST, Routes} from 'discord.js';
+import assert from 'assert';
+
+const {TOKEN, CLIENT_ID} = process.env;
+assert(TOKEN, 'TOKEN is not defined');
+assert(CLIENT_ID, 'CLIENT_ID is not defined');
 
 const commands = [
   {
-    name: "ping",
-    description: "Replies with Pong!",
+    name: 'ping',
+    description: 'Replies with Pong!',
   },
 ];
 
-const rest = new REST({ version: "10" }).setToken(TOKEN);
+const rest = new REST({version: '10'}).setToken(TOKEN);
 
 (async () => {
   try {
-    console.log("Started refreshing application (/) commands.");
+    console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+    await rest.put(Routes.applicationCommands(CLIENT_ID), {
+      body: commands,
+    });
 
-    console.log("Successfully reloaded application (/) commands.");
+    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
 })();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+client.login(TOKEN);
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready', () => {
+  const {user} = client;
+  assert(user, 'Login failed!!');
+
+  console.log(`Logged in as ${user.tag}!`);
 });
 
-client.on("interactionCreate", async (interaction) => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
   }
 });
-
-client.login(TOKEN);
